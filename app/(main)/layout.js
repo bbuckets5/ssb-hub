9da6276780ad5dfd26528e22db91feaab6f8a32a
bbuckets5 <95374981+bbuckets5@users.noreply.github.com>
2025-09-30 18:43:43 -0400

    same thing

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -7,7 +7,9 @@ export default function MainLayout({ children }) {
   return (
     <>
       <Header />
-      {children}
+      <main>
+        {children}
+      </main>
       <Footer />
     </>
   );
